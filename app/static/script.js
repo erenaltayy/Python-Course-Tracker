00a3668eeaac6df0e3ef44eed0e4ac541b2150c7
erenaltayy <85945886+erenaltayy@users.noreply.github.com>
2023-11-03 11:46:11 +0300

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,20 +1,43 @@
-const a_tags = document.getElementsByClassName("a_tag");
-let id = 1;
+const a_tags = Array.from(document.getElementsByClassName("a_tag"));
+let clickeds = [];
+
+let a_id = 1;
 a_tags.forEach((a) => {
-    a.id = id;
-    id++;
+    a.setAttribute("id", a_id);
+    a_id++;
 });
 
 
 document.querySelectorAll("a").forEach((a) => {
-    a.addEventListener("click", () => {
-        a.style.backgroundColor = green;
-        //sayfa yüklendiğinde nasıl kalacak
+    a.addEventListener("click", (event) => {
+        localStorage.setItem("id_"+ event.target.id, event.target.id);
+        console.log("local storage eklendi: " + event.target.id);
     });
 });
 
+//  let dizi = Object.keys(localStorage);
+//  console.log(dizi);
 
-//flasktaki cookiler daha kolay olabilir
 
+window.addEventListener("load", () => {
+    let clicked_keys = Object.keys(localStorage);
+    clicked_keys.forEach( (key) => {
+        console.log("eklenen key: " + key)
+        clickeds.push(key);
+    });
+   
 
-
+    clickeds.forEach( (c) => {
+        const id = c.match(/\d+/g);
+        let clicked_a_tag = document.getElementById(id);
+        if(clicked_a_tag){
+            clicked_a_tag.style.backgroundColor = "green";
+            clicked_a_tag.style.color = "white";
+        }
+        else{
+            console.log("aranan eleman bulunamadı");
+        }
+       
+    })
+    
+});
