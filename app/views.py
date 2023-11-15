aa0dee40427c4ea68dea1eede7e8d954dec75357
erenaltayy <erenaltay926@gmail.com>
2023-11-15 13:09:58 +0300

    added try catch

--- app/views.py
+++ app/views.py
@@ -5,21 +5,24 @@ import requests
 
 @app.route("/")
 def index():
-    url = "https://www.tutorialspoint.com/python/index.htm"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+    try: 
+        url = "https://www.tutorialspoint.com/python/index.htm"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    ul_tag = soup.find("ul", class_="toc chapters")
-    li_tags = ul_tag.find_all("li")
-    a_tags = []
-    for li_tag in li_tags:
-        a_tag = li_tag.find("a")
-        if a_tag is not None:
-            a_tag_url = f"https://www.tutorialspoint.com{a_tag.get('href')}"
-            a_tag_tittle = li_tag.get_text()
-            a_tag = {"title": a_tag_tittle, "url": a_tag_url}
-            a_tags.append(a_tag)
-        else:
-            continue
+        ul_tag = soup.find("ul", class_="toc chapters")
+        li_tags = ul_tag.find_all("li")
+        a_tags = []
+        for li_tag in li_tags:
+            a_tag = li_tag.find("a")
+            if a_tag is not None:
+                a_tag_url = f"https://www.tutorialspoint.com{a_tag.get('href')}"
+                a_tag_tittle = li_tag.get_text()
+                a_tag = {"title": a_tag_tittle, "url": a_tag_url}
+                a_tags.append(a_tag)
+            else:
+                continue
 
-    return render_template("index.html", a_tags = a_tags)
+        return render_template("index.html", a_tags = a_tags)
+    except:
+        return "The page failed to load."
